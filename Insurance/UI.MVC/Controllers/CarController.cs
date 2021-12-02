55bae79db5323509f174fbf1bfd3125038efd9c6
Maxim Derboven <maxim.derboven@student.kdg.be>
2021-12-02 20:53:24 +0100

    Added basis after W3 lesson

---
+++ Insurance/UI.MVC/Controllers/CarController.cs
@@ -0,0 +1,24 @@
+﻿using Insurance.BL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UI.MVC.Controllers
+{
+    public class CarController : Controller
+    {
+        public CarController()
+        {
+        }
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
